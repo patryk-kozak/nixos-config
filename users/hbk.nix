431b27bad2b4f185f4aa5e6277fde75edfeca92e
Patryk Kozak <thehackbee@gmail.com>
2022-10-27 18:17:17 +0200

    Update Discord to latest

--- users/hbk.nix
+++ users/hbk.nix
@@ -48,10 +48,10 @@ in {
       gcc
       jdk
       (discord.override {
-        version = "0.0.20";
+        version = "0.0.21";
         src = latest-nixpkgs.fetchurl {
-          url = "https://dl.discordapp.net/apps/linux/0.0.20/discord-0.0.20.tar.gz";
-          sha256 = "3f7yuxigEF3e8qhCetCHKBtV4XUHsx/iYiaCCXjspYw=";
+          url = "https://dl.discordapp.net/apps/linux/0.0.21/discord-0.0.21.tar.gz";
+          sha256 = "sha256-KDKUssPRrs/D10s5GhJ23hctatQmyqd27xS9nU7iNaM=";
         };
       })
     ];
